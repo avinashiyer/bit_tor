586c67ec8f2241939671111689f4f007736228fe
avinashiyer <avinash.h.iyer@gmail.com>
2023-09-12 17:43:24 -0500

    before lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,21 @@
+#[allow(dead_code)]
 mod bencode {
     use std::collections::HashMap;
-    #[derive(Debug)]
-    pub enum BencodeVal {
-        Message(String),
+    #[derive(Debug, PartialEq)]
+    pub enum BencodeVal<'a> {
+        Message(&'a str),
         Int(isize),
-        List(Vec<BencodeVal>),
-        Dict(HashMap<String, BencodeVal>),
+        List(Vec<BencodeVal<'a>>),
+        Dict(HashMap<String, BencodeVal<'a>>),
         Stop,
     }
 
-    impl BencodeVal {
+    impl BencodeVal<'a> {
         // Convenience method to decode a whole string and return all bencode values in a vec
         pub fn decode_str(src: &str) -> Vec<BencodeVal> {
             let mut vals = Vec::<BencodeVal>::new();
             let mut chars_indices = src.char_indices().peekable();
-            while let Some((_pos, ch_ref)) = chars_indices.peek() {
+            while chars_indices.peek().is_some() {
                 match Self::decode_dispatch(&mut chars_indices) {
                     BencodeVal::Stop => break,
                     x => vals.push(x),
@@ -26,7 +27,7 @@ mod bencode {
             mut chars_indices: &mut std::iter::Peekable<std::str::CharIndices<'a>>,
         ) -> BencodeVal {
             let mut val = BencodeVal::Stop;
-            while let Some((_pos, ch)) = chars_indices.peek() {
+            if let Some((_pos, ch)) = chars_indices.peek() {
                 val = match ch {
                     'e' => BencodeVal::Stop,
                     'i' => {
@@ -38,6 +39,10 @@ mod bencode {
                         chars_indices.next();
                         Self::decode_list(&mut chars_indices)
                     }
+                    'd' => {
+                        chars_indices.next();
+                        Self::decode_dict(&mut chars_indices)
+                    }
                     _ => {
                         panic!("Strange value in decode dispatch matched: {ch}")
                     }
@@ -46,7 +51,7 @@ mod bencode {
             val
         }
 
-        pub fn encode_str(src: BencodeVal) -> String {
+        pub fn encode_str(_src: BencodeVal) -> String {
             String::from("pass")
         }
         fn decode_int<'a>(
@@ -78,7 +83,6 @@ mod bencode {
                 .parse::<usize>()
                 .unwrap();
             // Take num chars from iter
-            println!("{num}");
             let s: String = chars_indices.take(num).map(|(_pos, c)| c).collect();
             if s.len() != num {
                 panic!(
@@ -173,6 +177,18 @@ mod bencode_tests {
     }
 
     #[test]
+    fn neg_int_decode() {
+        let num = -32;
+        let s = format!("i{num}e");
+        match BencodeVal::decode_dispatch(&mut s.char_indices().peekable()) {
+            BencodeVal::Int(n) => assert_eq!(n, num),
+            _ => {
+                assert!(false)
+            }
+        }
+    }
+
+    #[test]
     fn test_message_decode() {
         let s = String::from("12:Hello World!");
         let res = BencodeVal::decode_dispatch(&mut s.char_indices().peekable());
@@ -184,6 +200,71 @@ mod bencode_tests {
                 assert!(false)
             }
         }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_message_short() {
+        let s = String::from("12:hello ");
+        BencodeVal::decode_dispatch(&mut s.char_indices().peekable());
+    }
+
+    #[test]
+    fn test_message_with_bencoded_vals() {
+        let s = "35:abcd12:hello_world!li22ed3:eari45ee";
+        match BencodeVal::decode_dispatch(&mut s.char_indices().peekable()) {
+            BencodeVal::Message(val) => assert_eq!(s[3..], val),
+            _ => {
+                assert!(false)
+            }
+        }
+    }
+
+    #[test]
+    fn test_decode_multiple_vals_ints() {
+        let s = "i33ei-1e";
+        let v = BencodeVal::decode_str(s);
+        println!("{:?}", v);
+        match v[0] {
+            BencodeVal::Int(x) => assert_eq!(33, x),
+            _ => {
+                assert!(false)
+            }
+        }
+        match v[1] {
+            BencodeVal::Int(x) => assert_eq!(-1, x),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_decode_list_pos() {
+        let mut s = "li3e5:hi55ei8e0:e".char_indices().peekable();
+        let v = BencodeVal::decode_dispatch(&mut s);
+        let vec = match v {
+            BencodeVal::List(val) => val,
+            _ => panic!("{:?}", v),
+        };
+        assert_eq!(vec[0], BencodeVal::Int(3));
+        assert_eq!(vec[1], BencodeVal::Message(String::from("hi55e")));
+        assert_eq!(vec[2], BencodeVal::Int(8));
+        assert_eq!(vec[3], BencodeVal::Message(String::from("")));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_decode_list_no_end() {
+        let mut s = "li44ei4e4:abcd".char_indices().peekable();
+        BencodeVal::decode_dispatch(&mut s);
+    }
+    // TODO: Nested List
+
+    #[test]
+    fn test_dict_decode_pos() {
+        let mut s = "d3:cow3:moo4:spam4:eggse".char_indices().peekable();
+        let vals = HashMap::from([
+            ("cow",BencodeVal::Message("moo")),
+        ])
     }
 
     // #[test]
