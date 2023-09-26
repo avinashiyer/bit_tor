4688445c3d1f92754fb46bdef19105210000c2fc
avinashiyer <avinash.h.iyer@gmail.com>
2023-09-26 16:25:25 -0500

    got a http response from the tracker

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use reqwest;
 use std::collections::BTreeMap;
 use std::fs;
 use std::io::prelude::*;
+use std::net::IpAddr;
 
 // use std::thread;
 // use std::time::Duration;
@@ -31,20 +32,18 @@ fn main() -> std::io::Result<()> {
         .expect("No info value")
         .encode_val();
     let info_hash = sha1_smol::Sha1::from(info_bencoded).digest().bytes();
-    let escaped_info_hash = percent_encode(&info_hash, ESCAPED_CHARACTERS);
+    let escaped_info_hash = percent_encode(&info_hash, ESCAPED_CHARACTERS).to_string();
     let meta_info = MetaInfo::construct_from_dict_v1(root_dict);
-    let utf_8_str = std::str::from_utf8(&meta_info.announce)
-        .expect("Error converting announce url to utf-8 encoding");
-    // let
-    // let s = GetRequest {
-    //     compact:0,
-    //     downloaded:String::from("0"),
-    //     event: Some(String::from("started")),
-    //     info_hash:
-
-    //}
-
+    fun_name(meta_info, escaped_info_hash);
     Ok(())
+}
+
+fn fun_name(meta_info: MetaInfo, escaped_info_hash: String) {
+    let req = reqwest::blocking::get(make_get_request(&meta_info, escaped_info_hash))
+        .unwrap()
+        .text()
+        .unwrap();
+    println!("{req}");
 }
 
 fn _get_tracker_url(src: &BTreeMap<Vec<u8>, Bencode>) -> String {
@@ -56,4 +55,24 @@ fn _get_tracker_url(src: &BTreeMap<Vec<u8>, Bencode>) -> String {
     } else {
         panic!("Ill formatted torrent file, no \"announce\" key in root dictionary.");
     }
+}
+
+fn make_peer_id() -> String {
+    use rand::Rng;
+    let mut res = vec![b'-', b'A', b'V', b'0', b'0', b'0', b'1', b'-'];
+    let mut rng = rand::thread_rng();
+    for _ in 0..12 {
+        res.push(rng.gen_range(b'0'..b'9'));
+    }
+    String::from_utf8(res).unwrap()
+}
+
+fn make_get_request(meta: &MetaInfo, escaped_hash: String) -> String {
+    let announce_url_utf8 = std::str::from_utf8(&meta.announce)
+        .expect("Error converting announce url to utf-8 encoding");
+    let peer_id = make_peer_id();
+    let bytes_left = meta.info.file_length.unwrap().to_string();
+    let res = format!(
+        "{announce_url_utf8}?info_hash={escaped_hash}&peer_id={peer_id}&port=6881&uploaded=0&downloaded=0&left={bytes_left}&compact=1&event=started&");
+    res.to_string()
 }
