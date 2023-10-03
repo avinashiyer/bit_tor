e92c0f7cf742851da81640349d0bbb6dd0f97e22
avinashiyer <avinash.h.iyer@gmail.com>
2023-10-03 14:27:04 -0500

    worked on peer2peer a little

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,25 @@
 #![allow(dead_code)]
 
-use core::num;
 use std::collections::BTreeMap;
+use std::io::{Write, ErrorKind};
+use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
+
+use percent_encoding::{percent_encode, AsciiSet, NON_ALPHANUMERIC};
 
 use bencode::Bencode;
+use file_dict::FileDict;
 
 pub mod bencode;
 pub mod decode;
 pub mod file_dict;
 
-use file_dict::FileDict;
-use url::form_urlencoded::parse;
-// use reqwest::RequestBuilder;
 type ByteString = Vec<u8>;
+
+const ESCAPED_CHARACTERS: &AsciiSet = &NON_ALPHANUMERIC
+    .remove(b'.')
+    .remove(b'-')
+    .remove(b'_')
+    .remove(b'~');
 
 pub struct MetaInfo {
     pub announce: ByteString,
@@ -23,9 +30,15 @@ pub struct MetaInfo {
     pub encoding: Option<ByteString>,
     pub url_list: Option<Vec<ByteString>>,
     pub info: FileDict,
+    pub info_hash: [u8; 20],
+    pub escaped_hash: String,
 }
 impl MetaInfo {
-    pub fn construct_from_dict_v1(root_dict: BTreeMap<ByteString, Bencode>) -> MetaInfo {
+    pub fn construct_from_dict_v1(
+        root_dict: BTreeMap<ByteString, Bencode>,
+        hashed_info: [u8; 20],
+    ) -> MetaInfo {
+        let escaped_hash = percent_encode(&hashed_info, ESCAPED_CHARACTERS).to_string();
         MetaInfo {
             announce: Self::get_message(&root_dict, "announce".as_bytes()).unwrap(),
             // TODO: Implement announce-list
@@ -36,7 +49,31 @@ impl MetaInfo {
             url_list: Self::get_url_list(&root_dict),
             info: FileDict::construct_from_info(root_dict.get("info".as_bytes()).unwrap()),
             announce_list: None,
+            info_hash: hashed_info,
+            escaped_hash,
         }
+    }
+
+    pub fn tracker_get(meta_info: MetaInfo, peer_id: String) -> Vec<u8> {
+        let announce_url_utf8 = std::str::from_utf8(&meta_info.announce)
+            .expect("Error converting announce url to utf-8 encoding");
+        // let bytes_left = meta.info.file_length.unwrap().to_string();
+        let res = format!(
+            "{announce_url_utf8}?\
+            info_hash={escaped_hash}&\
+            event=started&\
+            peer_id={peer_id}\
+            &compact=\
+            &numwant=5",
+            escaped_hash = meta_info.escaped_hash
+        );
+        let response: Vec<u8> = match reqwest::blocking::get(res) {
+            Ok(rep) => rep.bytes().unwrap().iter().map(|x| *x).collect(),
+            Err(e) => {
+                panic!("{e}")
+            }
+        };
+        response
     }
 
     fn get_message(d: &BTreeMap<ByteString, Bencode>, key: &[u8]) -> Option<ByteString> {
@@ -56,55 +93,80 @@ impl MetaInfo {
     }
 }
 
-pub struct GetRequest {
-    pub compact: u8,
-    pub downloaded: String,
-    pub event: Option<String>,
-    pub info_hash: ByteString,
-    pub ip: Option<String>,
-    pub key: Option<ByteString>,
-    pub left: String,
-    pub pubnumwant: Option<usize>,
-    pub peer_id: ByteString,
-    pub port: String,
-    pub trackerid: Option<ByteString>,
-    pub uploaded: String,
+#[derive(Debug)]
+pub struct Peer {
+    pub am_choking: u8,
+    pub am_interested: u8,
+    pub peer_choking: u8,
+    pub peer_interested: u8,
+    pub socket: SocketAddrV4,
+    pub handle: TcpStream,
 }
 
-// impl GetRequest {
-//     pub fn make_get_reqwest_body(&self) -> RequestBuilder {
-//         RequestBuilder::body(self, body);
-//     }
-// }
-
-pub struct TrackerResponse {
-    pub warning_reason: Option<String>,
-    pub interval: usize,
-    pub min_interval: Option<usize>,
-    pub tracker_id: Option<ByteString>,
-    pub complete: isize,
-    pub incomplete: isize,
-}
-
-// impl TrackerResponse {
-//     pub fn parse_response_string(response:String) -> TrackerResponse {
-//         let dict = Bencode::decode_all(response.as_bytes()).get(0).unwrap();
-//         TrackerResponse { warning_reason: None, interval: 900, min_interval: None, tracker_id: None, complete: 1, incomplete: 1 }
-//     }
-// }
-
-use std::net::{SocketAddrV4,Ipv4Addr};
-pub fn extract_peers_from_compact_response(bytes: Vec<u8>) -> Vec<SocketAddrV4> {
-    if bytes.len() % 6 != 0 {
-        panic!("Comapct peers byte string is not a multiple of 6. Impossible to parse");
+impl Peer {
+    pub fn new_peer(socket: SocketAddrV4) -> Option<Peer> {
+        let handle = match TcpStream::connect_timeout(
+            &std::net::SocketAddr::V4(socket),
+            std::time::Duration::new(5, 0),
+        ) {
+            Ok(h) => h,
+            Err(e) => return None,
+        };
+        Some(Peer {
+            am_choking: 1,
+            am_interested: 0,
+            peer_choking: 1,
+            peer_interested: 0,
+            socket,
+            handle,
+        })
     }
-    let num_addrs = bytes.len() / 6;
-    let mut parsed_peers = Vec::<SocketAddrV4>::with_capacity(num_addrs);
-    let mut it = bytes.into_iter();
-    for _ in 0..num_addrs {
-        let x:Vec<u8> = it.by_ref().take(6).collect();
-        let port = u16::from_be_bytes([x[4],x[5]]);
-        parsed_peers.push(SocketAddrV4::new(Ipv4Addr::new(x[0], x[1], x[2], x[3]), port));
+
+    pub fn get_peers(response: Vec<u8>) -> Vec<Peer> {
+        let mut response_iter = response.iter().peekable();
+        let bencoded_response = Bencode::decode_single(&mut response_iter);
+        let tracker_response_dict = bencoded_response.unwrap_dict();
+        if let Some(x) = tracker_response_dict.get("failure reason".as_bytes()) {
+            panic!(
+                "Tracker Request Failed with reason: \n{}",
+                String::from_utf8_lossy(&x.unwrap_message())
+            );
+        }
+        match tracker_response_dict.get("peers".as_bytes()) {
+            None => panic!("No peers in tracker respone"),
+            Some(Bencode::Dict(_)) => {
+                panic!("Non Compact response from tracker recieved")
+            }
+            Some(Bencode::Message(m)) => Peer::extract_peers_from_compact_response(m.to_vec()),
+            _ => panic!("Unexpected value in peers"),
+        }
     }
-    parsed_peers
+
+    pub fn extract_peers_from_compact_response(bytes: Vec<u8>) -> Vec<Peer> {
+        if bytes.len() % 6 != 0 {
+            panic!("Comapct peers byte string is not a multiple of 6. Impossible to parse");
+        }
+        let num_addrs = bytes.len() / 6;
+        let mut parsed_peers = Vec::<SocketAddrV4>::with_capacity(num_addrs);
+        let mut it = bytes.into_iter();
+        for _ in 0..num_addrs {
+            let x: Vec<u8> = it.by_ref().take(6).collect();
+            let port = u16::from_be_bytes([x[4], x[5]]);
+            parsed_peers.push(SocketAddrV4::new(
+                Ipv4Addr::new(x[0], x[1], x[2], x[3]),
+                port,
+            ));
+        }
+        parsed_peers
+            .into_iter()
+            .map(|skt| Peer::new_peer(skt))
+            .flatten()
+            .collect()
+    }
+
+    pub fn send_to_peer(&mut self, message: &[u8]) {
+        if let Err(e) = self.handle.write_all(message) {
+            panic!("{e} \n\n occured when trying to write to a peer");
+        } 
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bit_tor::bencode::Bencode;
 use bit_tor::MetaInfo;
+use bit_tor::Peer;
 use core::panic;
-use percent_encoding::{percent_encode, AsciiSet, NON_ALPHANUMERIC};
 use reqwest;
 use std::collections::BTreeMap;
 use std::env;
@@ -10,19 +10,25 @@ use std::io::prelude::*;
 
 // use std::thread;
 // use std::time::Duration;
-const ESCAPED_CHARACTERS: &AsciiSet = &NON_ALPHANUMERIC
-    .remove(b'.')
-    .remove(b'-')
-    .remove(b'_')
-    .remove(b'~');
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let mut file = fs::File::open(
+    let file = fs::File::open(
         args.get(1)
             .expect("No file supplied in command line invocation"),
     )
     .unwrap();
+    let peer_id = make_peer_id();
+    let (root_dict, info_bencoded) = read_torrent(file);
+    let hashed_info = sha1_smol::Sha1::from(info_bencoded).digest().bytes();
+    let meta_info = MetaInfo::construct_from_dict_v1(root_dict, hashed_info);
+    let response = MetaInfo::tracker_get(meta_info, peer_id);
+    let peers = Peer::get_peers(response);
+    println!("{:#?}", peers);
+    Ok(())
+}
+
+fn read_torrent(mut file: fs::File) -> (BTreeMap<Vec<u8>, Bencode>, Vec<u8>) {
     let mut buf = Vec::with_capacity(1_000_000);
     let _bytes_read = file.read_to_end(&mut buf);
     let root_dict = match Bencode::decode_single(&mut buf.iter().peekable()) {
@@ -33,46 +39,16 @@ fn main() -> std::io::Result<()> {
         .get("info".as_bytes())
         .expect("No info value")
         .encode_val();
-    let info_hash = sha1_smol::Sha1::from(info_bencoded).digest().bytes();
-    let escaped_info_hash = percent_encode(&info_hash, ESCAPED_CHARACTERS).to_string();
-    let meta_info = MetaInfo::construct_from_dict_v1(root_dict);
-    let response: Vec<u8> =
-        match reqwest::blocking::get(make_get_request(&meta_info, escaped_info_hash)) {
-            Ok(rep) => rep.bytes().unwrap().iter().map(|x| *x).collect(),
-            Err(e) => {
-                panic!("{e}")
-            }
-    };
-    let mut response_iter = response.iter().peekable();
-    let binding = Bencode::decode_single(&mut response_iter);
-    let tracker_response_dict =
-        binding.unwrap_dict();
-    println!("{:?}",binding);
-    if let Some(x) = tracker_response_dict.get("failure reason".as_bytes()) {
-        panic!("Tracker Request Failed with reason: \n{}",String::from_utf8_lossy(&x.unwrap_message()));
-    }
-    // match tracker_response_dict.get("peers") {
-    //     None => panic!("No peers in tracker respone"),
-    //     Some(Bencode::Dict(d)) => {panic!("todo")},
-    //     Some(Bencode::Message())
-    // }
-    Ok(())
+    (root_dict, info_bencoded)
 }
 
-fn _get_tracker_url(src: &BTreeMap<Vec<u8>, Bencode>) -> String {
-    if let Some(url_bencode) = src.get("announce".as_bytes()) {
-        if let Bencode::Message(url) = url_bencode {
-            return String::from_utf8(url.clone()).expect("Invalid Url in announce field");
-        }
-        panic!("Value under \"announce\" key is not a bencoded string.")
-    } else {
-        panic!("Ill formatted torrent file, no \"announce\" key in root dictionary.");
-    }
-}
+
 
 fn make_peer_id() -> String {
+    const PEER_TAG: [u8; 8] = *b"-AV0001-";
     use rand::Rng;
-    let mut res = vec![b'-', b'A', b'V', b'0', b'0', b'0', b'1', b'-'];
+    let mut res = Vec::new();
+    res.extend(PEER_TAG);
     let mut rng = rand::thread_rng();
     for _ in 0..12 {
         res.push(rng.gen_range(b'0'..=b'9'));
@@ -80,13 +56,19 @@ fn make_peer_id() -> String {
     String::from_utf8(res).unwrap()
 }
 
-fn make_get_request(meta: &MetaInfo, escaped_hash: String) -> String {
-    let announce_url_utf8 = std::str::from_utf8(&meta.announce)
-        .expect("Error converting announce url to utf-8 encoding");
-    let peer_id = make_peer_id();
-    // let bytes_left = meta.info.file_length.unwrap().to_string();
-    let res = format!(
-        "{announce_url_utf8}?info_hash={escaped_hash}&event=started&peer_id={peer_id}&compact=1&numwant=5"
-    );
-    res
+
+
+//len(info_hash) + len(peer_id) + 8 reserved bytes + 1 byte declaring length of protocol string
+const BASE_HANDSHAKE_LENGTH: u8 = 20 + 20 + 8 + 1;
+fn initial_request(meta: MetaInfo, peer_id: String) -> Vec<u8> {
+    let pstr = "BitTorrent protocol".as_bytes();
+    let pstr_len = pstr.len() as u8;
+    let mut raw_bytes = Vec::<u8>::with_capacity((BASE_HANDSHAKE_LENGTH + pstr_len) as usize);
+    raw_bytes.push(pstr_len);
+    raw_bytes.extend(pstr);
+    // Reserved Bytes
+    raw_bytes.extend([0u8; 8]);
+    raw_bytes.extend(meta.info_hash);
+    raw_bytes.extend(peer_id.as_bytes());
+    raw_bytes
 }
